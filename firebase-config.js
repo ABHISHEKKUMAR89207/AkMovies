bef494224201c4e9aab9f1f0718f3739db4367d1
ABHISHEKKUMAR89207 <abhibk9711place@gmail.com>
2023-09-14 00:37:06 +0530

    Merge remote changes

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD1YmnryjHQ8fSOHXaYp4s7vOm-I4ghBPg",
-    authDomain: "movies-700da.firebaseapp.com",
-    projectId: "movies-700da",
-    storageBucket: "movies-700da.appspot.com",
-    messagingSenderId: "400931630235",
-    appId: "1:400931630235:web:b393ec20feee54f9e62a3e",
-    measurementId: "G-E5E48TYM33"
-  };
-  
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_AUTH_DOMAIN",
+projectId: "YOUR_PROJECT_ID",
+storageBucket: "YOUR_STORAGE_BUCKET",
+messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+appId: "YOUR_APP_ID",
+measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
